e5362b02bb3a6c957fe082ab8b31bb43d5a625e3
TheJouster <tomilovdima999@gmail.com>
2024-10-11 17:12:11 +0500

    Task2

---
+++ Tyuiu.TomilovDA.Sprint2.Task2.V14.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+﻿namespace Tyuiu.TomilovDA.Sprint2.Task2.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
